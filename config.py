56bceeb30f974471a6c0b92c91b91c5c29224eaf
ixrocky <shivamlkr629@gmail.com>
2025-12-15 16:46:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ixrocky/Esvo",
+    "https://github.com/ixrocky/Lofi2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
