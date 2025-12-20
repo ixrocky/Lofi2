bf0ab377e047666bffaa64290294b93f5208424c
ixrocky <shivamlkr629@gmail.com>
2025-12-20 22:39:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 
 # Vars For API End Pont.
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_URL = getenv("API_URL", 'https://api2.nexgenbots.xyz') #youtube song url
 API_KEY = getenv("API_KEY", "NxGBNexGenBotsbb404a") # youtube song api key, get it from https://t.me/MR_ROCKY_TZ
 
 # Get this value from @MissRose_Bot on Telegram by /id
